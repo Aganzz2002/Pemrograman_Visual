a3d48e869ed56f61e5a6085f8b268c6d353be5a7
Aganzz2002 <aganzzjuandrometta@gmail.com>
2024-10-05 09:36:52 +0700

    first commit

---
+++ Variabel_TipeData/src/variabel_tipedata/Variabel_TipeData.java
@@ -0,0 +1,17 @@
+package variabel_tipedata;
+public class Variabel_TipeData {
+    public static void main(String[] args) {
+            
+            int var1 = 56;
+            double var2 = 0.543;
+            boolean var3 = true;
+            char var4 = 'A';
+            String var5 = "Belajar Java";
+            
+            System.out.println(var1);
+            System.out.println(var2);
+            System.out.println(var3);
+            System.out.println(var4);
+            System.out.println(var5);
+    }
+} 
